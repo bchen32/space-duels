79a0d2802b638238735767bf8df7c501671699f1
StormFalcon32 <benchen9708@gmail.com>
2021-01-14 11:40:59 -0500

    Steam

---
+++ SpaceShooterClient/Scenes/Singletons/Globals.gd
@@ -0,0 +1,27 @@
+extends Node
+
+# Steam variables
+var owned = false
+var online = false
+var steam_id = 0
+var steam_name = ''
+
+func _ready():
+	var init = Steam.steamInit()
+	print(init)
+	if init['status'] != 1:
+		print('Failed to initialize Steam. ' + str(init['verbal']))
+		get_tree().quit()
+	print('Initialized')
+	owned = Steam.isSubscribed()
+	online = Steam.loggedOn()
+	steam_id = Steam.getSteamID()
+	steam_name = Steam.getPersonaName()
+	
+	# Check if account owns the game
+	if not owned:
+		print('Game not owned')
+		get_tree().quit()
+
+func _process(_delta):
+	Steam.run_callbacks()
